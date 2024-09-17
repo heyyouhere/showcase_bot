d15feaaebabf90d169ed12d68bf4711ca3b48c9e
heyyouhere <heyyouhere@v144372.hosted-by-vdsina.com>
2024-09-17 14:33:34 +0300

    Initial commit. Implemented function to generate openai deskription.

---
+++ description_gen.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+import base64
+import requests
+import os
+api_key= os.getenv("OPENAI_TOKEN")
+
+def gererate_ai_description(image_path : str, prompt: str) -> str:
+    # Getting the base64 string
+    with open(image_path, "rb") as image_file:
+        base64_image =  base64.b64encode(image_file.read()).decode('utf-8')
+    headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {api_key}"
+    }
+    payload = {
+      "model": "gpt-4o",
+      "messages": [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": 'text',
+              "text": prompt
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+              }
+            }
+          ]
+        }
+      ],
+      "max_tokens": 300
+    }
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    print(response.json())
+    result = response.json()['choices'][0]['message']['content']
+    return result
+
+if __name__ == '__main__':
+    image_path = "./photo.jpeg"
+    prompt = 'Я отправлю тебе фотографию, твоя задача сгенерировать ироничную фразу используя контекст фотографии по типу "Я еду на работу, пока все смотрят Nornikel digital week". Фраза должна заканчиваться на "Пока все смотрят Nornikel digital week" и должна быть достаточно короткой, ответь только этой фразой ничего больше не пиши. Фраза ни в коем случае не должна осквернять или быть негативный в сторону Норникеля. Фраза должна быть позитивной.'
+    res = gererate_ai_description(image_path, prompt)
+    print("Result:", res)
