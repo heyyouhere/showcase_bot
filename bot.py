3e9a1c82bc3793d61f15f7d6f5d3e22847f9cb92
heyyouhere <max.kalan@gmail.com>
2024-09-17 18:50:44 +0300

    Resizing image. TODO: Make image in memory, no disk save

--- bot.py
+++ bot.py
@@ -1,14 +1,55 @@
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
 import os
 import requests
+from PIL import Image, ImageDraw, ImageFont
 import description_gen
+
 
 TOKEN = os.getenv("TG_TOKEN")
 
 prompt = 'Я отправлю тебе фотографию, твоя задача сгенерировать ироничную фразу используя контекст фотографии по типу "Я еду на работу, пока все смотрят Nornikel digital week". Фраза должна заканчиваться на "Пока все смотрят Nornikel digital week" и должна быть достаточно короткой, ответь только этой фразой ничего больше не пиши. Фраза ни в коем случае не должна осквернять или быть негативный в сторону Норникеля. Фраза должна быть позитивной.'
 
+target_size=(1024, 1024)
+
 async def start(update, context):
     await update.message.reply_text("Привет! Отправь фотку и получи смешное описание")
+
+def drawText(draw, font, text : str, position : (int, int)):
+    text_color = (255, 255, 255)
+    text_size = draw.textbbox((0,0), text, font=font)
+    draw.multiline_text(position, text, fill=text_color, font=font)
+
+
+mask_path = 'mask.png'
+mask_image = Image.open(mask_path)
+font_path = 'afuturaortobold.ttf'
+
+
+
+
+
+def create_card(image_path, desc):
+    background_image = Image.open(image_path)
+
+    size = background_image.size
+    x = min(size[0], size[1])
+    factor = target_size[0]/x
+    new_size = (int(size[0]*factor), int(size[1]*factor))
+    background_image = background_image.resize(new_size)
+    if new_size[0] == target_size[0]:
+        crop_box = (0, (new_size[1] - target_size[1])/2, new_size[0],  new_size[1]/2 + target_size[1]/2)
+    else:
+        crop_box = ((new_size[0] - target_size[0])/2, 0,  new_size[0]/2 + target_size[0]/2, new_size[1])
+    background_image = background_image.crop(crop_box)
+
+    background_image.paste(mask_image, (0, 0), mask_image)
+    draw = ImageDraw.Draw(background_image)
+    font = ImageFont.truetype(font_path, size=36)
+    drawText(draw, font, desc.upper(), (60, 920))
+    output_path= "output.png"
+    background_image.save("output.png")
+    return output_path
+
 
 
 async def send_description(update, context):
@@ -21,10 +62,10 @@ async def send_description(update, context):
     image_path = os.path.join("./inputs", file_name)
     with open(image_path, "wb") as out_file:
         out_file.write(r.content)
-    desc = description_gen.gererate_ai_description(image_path, prompt)
-    # desc = "test_test_test"
-    # os.remove(path)
-    await update.effective_user.send_message(desc)
+    #desc = description_gen.gererate_ai_description(image_path, prompt)
+    desc = "Я учу секреты продуктивности, пока все смотрят Nornikel digital week."
+    output_path = create_card(image_path, desc)
+    await update.effective_user.send_photo(photo=open(output_path, 'rb'), caption=desc)
 
 
 def main():
